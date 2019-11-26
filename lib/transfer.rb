e025c2e119dae0d7df29b9e09a56034aadb00aa7
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-26 22:14:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,14 @@ class Transfer
        self.sender.balance -= amount
        self.receiver.balance += amount
        self.status = "complete"
-    elsif amount > self.sender.balance
+    else 
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
-  end 
+  else 
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+    end 
   end
   
 end
